8d805cc6f451df355137c6c27dcf0477c7e044d9
Skvall <antonalizee@yandex.ru>
2022-11-29 12:41:18 +0300

    Подготовка к старту на сервере

--- config/settings.py
+++ config/settings.py
@@ -137,15 +137,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+if ENV_TYPE == 'local':
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 # Media files
 MEDIA_URL = '/media/'
 
